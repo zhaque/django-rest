ec7ebc9eaa62a6d595bd689ece2346fcacb8c494
CrowdSense <admin@DevBook.gateway.2wire.net>
2010-02-24 15:09:41 +0000

    updated package name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup
 
-setup(name='Djangorestmodel',
-      version='0.3',
+setup(name='django-rest',
+      version='0.4',
       description='XML backed models queried from external REST apis',
       author='Chris Tarttelin and Cam McHugh',
       author_email='ctarttelin@point2.com',
